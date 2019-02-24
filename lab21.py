879886311fc0400daf77fcfa8c55a591fb4eaa82
MinatoNamikaze2005 <47950858+MinatoNamikaze2005@users.noreply.github.com>
2019-02-24 21:11:30 +0200

    Add files via upload

---
+++ lab21.py
@@ -0,0 +1,15 @@
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+second_name = names[1]
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append("Hello")
+strings.append("world")
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
